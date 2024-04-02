fc2bf69bc34e34ff0094ccbd73d5d9683d40ed8f
ffffhx <17862926305@163.com>
2024-04-02 22:10:15 +0800

    test6

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import elementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(elementPlus)
 app.mount('#app')
