de3937a58eea74d356bf48ce3b29786ad268e5d6
ffffhx <17862926305@163.com>
2024-04-03 11:26:29 +0800

    2024/4/3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useDark, useToggle, useColorMode } from '@vueuse/core'
+
+const isDark = useDark() //true or false
+const colorMode = useColorMode({
+  modes: {
+    dim: 'dim',
+    cafe: 'cafe'
+  },
+});//'light' or 'dark'
+const toggleDark = useToggle(isDark)
+
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-6
-    </div>
-  </header>
-
+  {{ isDark }}
+  <br>
+  <button @click="toggleDark()">Toggle Daek Mode</button>
+  <br>
+  {{ colorMode }}
+  <button @click="colorMode = 'dark'">Dark Mode</button>
+  <button @click="colorMode = 'light'">light Mode</button>
+  <button @click="colorMode = 'dim'">Dim Mode</button>
+  <button @click="colorMode = 'cafe'">cafe Mode</button>
+  <p>123456</p>
+  <p>1611213216</p>
+  <p>1515656</p>
+  <p>8447845621</p>
+  <p>527456254</p>
 </template>
 
-<style scoped>
+<style scoped lang="less">
+
+.dark {
+  background: gray;
+  color: pink
+}
+
+.dim {
+  background: aqua;
+}
+
 
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import elementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import elementPlus from 'element-plus'//引入element组件库
+import 'element-plus/dist/index.css'//引入element组件库
 import App from './App.vue'
 import router from './router'
-
+import 'element-plus/theme-chalk/dark/css-vars.css'//暗黑模式主题切换
 const app = createApp(App)
 
 app.use(createPinia())
